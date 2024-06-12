044f80f638f644a34184c173f8a2f8eb0b9f2409
Luiz Felipe De Souza Marques <luizfelipesoma@gmail.com>
2024-06-12 17:27:46 -0300

    Modificando a pagina dos produtos

--- app/assets/_Componentes/produtos/page.js
+++ app/assets/_Componentes/produtos/page.js
@@ -1,17 +1,27 @@
 import produtos from "./produtos";
 import "./style_produtos.scss";
+import Image from "next/image";
 
 export default function Produtos() {
-
   return (
     <>
-      <div>
+      <div id="divDosProdutos">
         {produtos.map((item) => (
-          <div id={item.id} className="produto">
-            <div>imagem</div>
-            <h3>{item.nome}</h3>
-            <h4>{item.preco}</h4>
-            <h6>{item.descricao}</h6>
+          <div id={item.id} className="produto centro">
+            <div id="divSeparadaDoProduto" className="centro">
+              <div>
+                <Image src={item.img} width={120} height={120} />
+              </div>
+              <div id="textoDosProdutos" className="centro">
+                <div><h3 className="semMargem">{item.nome}</h3></div>
+                <div>
+                <h4 className="semMargem">R${item.preco},00</h4>
+                </div>
+                <div>
+                <h6 className="semMargem">{item.descricao}</h6>
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['i.pinimg.com'], // Substitua por seus nomes de host desejados
+      }
+};
 
 export default nextConfig;
